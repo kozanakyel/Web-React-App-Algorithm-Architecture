74042861c800c90e2cd52fcfa6ac791c3e1a86d8
kozanakyel <kozanakyel@gmail.com>
2023-09-05 23:06:01 +0200

    ited usecintext reducer and and other effects for food app finished at the fisrt stage

--- react-meal-app/src/components/Meals/MealItem/MealItemForm.js
+++ react-meal-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,19 +1,50 @@
-import classes from './MealItemForm.module.css';
-import Input from '../../UI/Input';
+import { useRef, useState } from 'react';
 
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
-    return <form className={classes.form}>
-        <Input label='Amount' input={{
-            id:'amount_' + props.id,
-            type:'number',
-            min:'1',
-            max:'5',
-            step:'1',
-            defaultValue:'1'
-        }} />
-        <button>+ Add</button>
-    </form>
-}
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+    // actually useRef dont wotk with custom components dont forget
+    // use React.forwardref in custom component like the <Input />
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;  // convert string to number in JS
+
+        if (
+            enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 5
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label='Amount'
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1',
+                }}
+            />
+            <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
+        </form>
+    );
+};
 
 export default MealItemForm;
--- react-meal-app/src/components/UI/Input.js
+++ react-meal-app/src/components/UI/Input.js
@@ -1,12 +1,15 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-
-const Input = (props) => {
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input} />
-    </div>
-}
-
+//useRef custom component ile ancak bu sekidle kullanilir
+const Input = React.forwardRef((props, ref) => {
+    return (
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input ref={ref} {...props.input} />
+        </div>
+    );
+});
 
 export default Input;
